e6872333fa7fa2cce4c9041033f27e775bf712a6
tianlinxu312 <t.xu12@lse.ac.uk>
2021-05-17 16:55:11 +0100

    delete download link

--- data/kth.py
+++ data/kth.py
@@ -1,56 +1,62 @@
 import random
 import os
 import numpy as np
-import socket
 import torch
 from scipy import misc
 from torch.utils.serialization import load_lua
 
-class KTH(object):
 
-    def __init__(self, train, data_root, seq_len = 20, image_size=64):
-        self.data_root = '%s/KTH/processed/' % data_root
+class KTH(object):
+    def __init__(self, train, data_root, seq_len=20, image_size=64):
+        self.data_root = '../data/kth'
         self.seq_len = seq_len
         self.image_size = image_size 
         self.classes = ['boxing', 'handclapping', 'handwaving', 'jogging', 'running', 'walking']
 
         self.dirs = os.listdir(self.data_root)
-        if train:
-            self.train = True
-            data_type = 'train'
-            self.persons = list(range(1, 21))
-        else:
-            self.train = False
-            self.persons = list(range(21, 26))
-            data_type = 'test'
-
-        self.data= {}
-        for c in self.classes:
-            self.data[c] = load_lua('%s/%s/%s_meta%dx%d.t7' % (self.data_root, c, data_type, image_size, image_size))
-     
 
         self.seed_set = False
 
     def get_sequence(self):
-        t = self.seq_len
-        while True: # skip seqeunces that are too short
-            c_idx = np.random.randint(len(self.classes))
-            c = self.classes[c_idx]
-            vid_idx = np.random.randint(len(self.data[c]))
-            vid = self.data[c][vid_idx]
-            seq_idx = np.random.randint(len(vid['files']))
-            if len(vid['files'][seq_idx]) - t >= 0:
-                break
-        dname = '%s/%s/%s' % (self.data_root, c, vid['vid'])
-        st = random.randint(0, len(vid['files'][seq_idx])-t)
-           
+        path = '../data/kth'
+        list_f = [x for x in os.listdir(path)]
 
-        seq = [] 
-        for i in range(st, st+t):
-            fname = '%s/%s' % (dname, vid['files'][seq_idx][i])
-            im = misc.imread(fname)/255.
-            seq.append(im[:, :, 0].reshape(self.image_size, self.image_size, 1))
-        return np.array(seq)
+        for x in range(batch_size):
+            rand_folder = random.choice(list_f)
+            path_to_file = path + '/' + rand_folder
+            file_name = random.choice(os.listdir(path_to_file))
+            path_to_video = path_to_file + '/' + file_name
+            vidcap = cv2.VideoCapture(path_to_video)
+            n_frames = vidcap.get(7)
+            frame_rate = vidcap.get(5)
+            ret, frame = vidcap.read()
+            # print(n_frames, rand_folder, file_name, frame_rate, frame.shape)
+            stacked_frames = []
+            while vidcap.isOpened():
+                frame_id = vidcap.get(1)  # current frame number
+                ret, frame = vidcap.read()
+                if not ret or len(stacked_frames) > (time_step - 1):
+                    break
+                frame = frame / 255.0
+                if rand_folder == 'running' or rand_folder == 'walking' or rand_folder == 'jogging':
+                    if frame_id % 1 == 0 and frame_id > 5:
+                        resized_frame = cv2.resize(frame, size=[height, width], interpolation='INTER_NEAREST')
+                elif n_frames < 350:
+                    if frame_id % 1 == 0 and frame_id > 5:
+                        resized_frame = cv2.resize(frame, size=[height, width], interpolation='INTER_NEAREST')
+                else:
+                    if frame_id % 1 == 0 and frame_id > 10:
+                        resized_frame = cv2.resize(frame, size=[height, width], interpolation='INTER_NEAREST')
+
+                stacked_frames.append(resized_frame)
+
+                if len(stacked_frames) < time_step:
+                    continue
+
+        stacked_frames = np.reshape(stacked_frames, newshape=(time_step, height, width, 3))
+        stacked_frames = np.transpose(stacked_frames, (1, 0, 2, 3))
+        stacked_frames = np.reshape(stacked_frames, newshape=(height, time_step * width, 3))
+        return stacked_frames
 
     def __getitem__(self, index):
         if not self.seed_set:
@@ -62,4 +68,3 @@ class KTH(object):
 
     def __len__(self):
         return len(self.dirs)*36*5 # arbitrary
-
