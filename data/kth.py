318c5272565de2ef530a26ce634a18801314592b
tianlinxu312 <t.xu12@lse.ac.uk>
2021-05-17 21:55:35 +0100

    fix data loader

--- data/kth.py
+++ data/kth.py
@@ -8,7 +8,7 @@ from torch.utils.serialization import load_lua
 
 class KTH(object):
     def __init__(self, train, data_root, seq_len=20, image_size=64):
-        self.data_root = '../data/kth'
+        self.path = data_root
         self.seq_len = seq_len
         self.image_size = image_size 
         self.classes = ['boxing', 'handclapping', 'handwaving', 'jogging', 'running', 'walking']
@@ -21,41 +21,35 @@ class KTH(object):
         path = '../data/kth'
         list_f = [x for x in os.listdir(path)]
 
-        for x in range(batch_size):
-            rand_folder = random.choice(list_f)
-            path_to_file = path + '/' + rand_folder
-            file_name = random.choice(os.listdir(path_to_file))
-            path_to_video = path_to_file + '/' + file_name
-            vidcap = cv2.VideoCapture(path_to_video)
-            n_frames = vidcap.get(7)
-            frame_rate = vidcap.get(5)
+        rand_folder = random.choice(list_f)
+        path_to_file = path + '/' + rand_folder
+        file_name = random.choice(os.listdir(path_to_file))
+        path_to_video = path_to_file + '/' + file_name
+        vidcap = cv2.VideoCapture(path_to_video)
+        n_frames = vidcap.get(7)
+        frame_rate = vidcap.get(5)
+        ret, frame = vidcap.read()
+        # print(n_frames, rand_folder, file_name, frame_rate, frame.shape)
+        stacked_frames = []
+        while vidcap.isOpened():
+            frame_id = vidcap.get(1)  # current frame number
             ret, frame = vidcap.read()
-            # print(n_frames, rand_folder, file_name, frame_rate, frame.shape)
-            stacked_frames = []
-            while vidcap.isOpened():
-                frame_id = vidcap.get(1)  # current frame number
-                ret, frame = vidcap.read()
-                if not ret or len(stacked_frames) > (time_step - 1):
-                    break
-                frame = frame / 255.0
-                if rand_folder == 'running' or rand_folder == 'walking' or rand_folder == 'jogging':
-                    if frame_id % 1 == 0 and frame_id > 5:
-                        resized_frame = cv2.resize(frame, size=[height, width], interpolation='INTER_NEAREST')
-                elif n_frames < 350:
-                    if frame_id % 1 == 0 and frame_id > 5:
-                        resized_frame = cv2.resize(frame, size=[height, width], interpolation='INTER_NEAREST')
-                else:
-                    if frame_id % 1 == 0 and frame_id > 10:
-                        resized_frame = cv2.resize(frame, size=[height, width], interpolation='INTER_NEAREST')
+            if not ret or len(stacked_frames) > (time_step - 1):
+                break
+            frame = frame / 255.0
+            if rand_folder == 'running' or rand_folder == 'walking' or rand_folder == 'jogging':
+                if frame_id % 1 == 0 and frame_id > 5:
+                    resized_frame = cv2.resize(frame, size=[height, width], interpolation='INTER_NEAREST')
+            elif n_frames < 350:
+                if frame_id % 1 == 0 and frame_id > 5:
+                    resized_frame = cv2.resize(frame, size=[height, width], interpolation='INTER_NEAREST')
+            else:
+                if frame_id % 1 == 0 and frame_id > 10:
+                    resized_frame = cv2.resize(frame, size=[height, width], interpolation='INTER_NEAREST')
 
-                stacked_frames.append(resized_frame)
-
-                if len(stacked_frames) < time_step:
-                    continue
+            stacked_frames.append(resized_frame)
 
         stacked_frames = np.reshape(stacked_frames, newshape=(time_step, height, width, 3))
-        stacked_frames = np.transpose(stacked_frames, (1, 0, 2, 3))
-        stacked_frames = np.reshape(stacked_frames, newshape=(height, time_step * width, 3))
         return stacked_frames
 
     def __getitem__(self, index):
@@ -63,7 +57,6 @@ class KTH(object):
             self.seed_set = True
             random.seed(index)
             np.random.seed(index)
-            #torch.manual_seed(index)
         return torch.from_numpy(self.get_sequence())
 
     def __len__(self):
